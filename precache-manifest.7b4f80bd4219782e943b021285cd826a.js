7c3f00a8b417d0f20c4919be667b08d9757be54b
IvanVFedenko <ivan.v.fedenko@gmail.com>
2019-08-04 19:09:28 +0300

    Updates

---
+++ precache-manifest.7b4f80bd4219782e943b021285cd826a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d55aadc72d36ff0a852ebe5a9f520d3b",
+    "url": "/react_pagination/index.html"
+  },
+  {
+    "revision": "bcfc833df90ca3c2ba6f",
+    "url": "/react_pagination/static/css/main.f6d1e940.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/react_pagination/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "bcfc833df90ca3c2ba6f",
+    "url": "/react_pagination/static/js/main.4bf60b35.chunk.js"
+  },
+  {
+    "revision": "ce8de57aa29cef69bbf2",
+    "url": "/react_pagination/static/js/runtime~main.c851b861.js"
+  }
+]);
